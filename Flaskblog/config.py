f9e4df37cb4bc9df689bb762fd9c241b8f00da5e
kymarimka <67918500+kymarimka@users.noreply.github.com>
2021-09-21 10:26:34 +0700

    Update config.py

--- Flaskblog/config.py
+++ Flaskblog/config.py
@@ -9,5 +9,5 @@ class Config:
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = 'kymarimka@gmail.com'  #os.environ.get('EMAIL_USER')
-    MAIL_PASSWORD = 'kymarimkaLOL'    #os.environ.get('EMAIL_PASS')
+    MAIL_USERNAME = 'WRITE-YOURS'  #os.environ.get('EMAIL_USER')
+    MAIL_PASSWORD = 'WRITE-YOURS'    #os.environ.get('EMAIL_PASS')
